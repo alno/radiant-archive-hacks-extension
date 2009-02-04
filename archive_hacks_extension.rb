a79df157561df0470d9c83999245bd10531c0e75
alno <alexey.noskov@gmail.com>
2009-02-04 23:59:18 +0300

    Gropping from Radiant::Config

--- archive_hacks_extension.rb
+++ archive_hacks_extension.rb
@@ -1,10 +1,9 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
+require_dependency 'application'
 
 class ArchiveHacksExtension < Radiant::Extension
   version "1.0"
   description "Describe your extension here"
-  url "http://yourwebsite.com/archive_hacks"
+  url "http://github.com/alno/radiant-archive-hacks-extension"
   
   # define_routes do |map|
   #   map.connect 'admin/archive_hacks/:action', :controller => 'admin/archive_hacks'
@@ -12,6 +11,20 @@ class ArchiveHacksExtension < Radiant::Extension
   
   def activate
     # admin.tabs.add "Archive Hacks", "/admin/archive_hacks", :after => "Layouts", :visibility => [:all]
+
+    ArchivePage.class_eval do # Hacking ArchivePage class
+
+      # Use groupping from Radiant::Config
+      def child_url(child)
+        date = child.published_at || Time.now
+        clean_url "#{ url }/#{ date.strftime archive_groupping }/#{ child.slug }"
+      end
+
+      def archive_groupping        
+        Radiant::Config['archive.groupping'] || Radiant::Config['archive.groupping'] = '%Y/%m'
+      end
+
+    end
   end
   
   def deactivate

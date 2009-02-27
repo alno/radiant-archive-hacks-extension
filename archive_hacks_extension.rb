89e127c8e6e94523110ea2838f10f6e7ee73ffc8
Alexey Noskov <alexey.noskov@gmail.com>
2009-02-27 11:02:01 +0300

    Revert "Refactoring"
    This reverts commit de2e8ddeec264fb4ed5fd9d0f813739d5b381949.

--- archive_hacks_extension.rb
+++ archive_hacks_extension.rb
@@ -7,7 +7,40 @@ class ArchiveHacksExtension < Radiant::Extension
     Radiant::Config['archive.group'] = '%Y/%m/%d' unless Radiant::Config['archive.group']
     Radiant::Config['archive.nogroup'] = '^\.(.+)$' unless Radiant::Config['archive.nogroup']
 
-    ArchivePage.send :include, ArchivePageHacks # Hacking ArchivePage class
+    # Hacking ArchivePage class
+    # Use groupping from Radiant::Config['archive.group'], default '%Y/%m'
+    # Don't use groupping for pages with slug, accepted by Radiant::Config['archive.nogroup'], default '\.(.*)'
+    ArchivePage.class_eval do # Hacking ArchivePage class
+
+      def child_url(child)
+        date = child.published_at || Time.now
+
+        if nogroup_slug = option_nogroup.match( child.slug )
+          clean_url "#{ url }/#{ nogroup_slug[1] }"
+        else
+          clean_url "#{ url }/#{ date.strftime option_group }/#{ child.slug }"
+        end
+      end
+
+      def option_group
+        @@option_group ||= Radiant::Config['archive.group']
+      end
+
+      def option_nogroup
+        @@option_nogroup ||= Regexp.new( Radiant::Config['archive.nogroup'] )
+      end
+
+      # Fixing placement archive as root page
+
+      alias_method :old_find_by_url, :find_by_url
+
+      # This method differs from original only by default value of last argument
+      def find_by_url(url, live = true, clean = true)
+        old_find_by_url(url, live, clean)
+      end
+
+    end
+
   end
   
   def deactivate
